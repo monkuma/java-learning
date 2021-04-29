4fc79832749ae64efd97ec3253682aa33b9aeec4
monkuma <kumamoto.jobs@gmail.com>
2021-04-29 19:18:06 +0900

    updates

--- learning-web/src/model/LectureLogic.java
+++ learning-web/src/model/LectureLogic.java
@@ -3,8 +3,18 @@ package model;
 public class LectureLogic {
 	private String lecture = "lecture";
 
-	public String execute(String no) {
-		this.lecture += no + ".jsp";
+	public String execute(String pageNo) {
+
+		try {
+			int page = Integer.parseInt(pageNo);
+			this.lecture += page + ".jsp";
+
+		}catch(Exception e){
+
+			this.lecture = "lectureTop.jsp";
+		}finally {
+
+		}
 		return lecture;
 	}
 
