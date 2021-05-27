8682050b63780d2ab8cb31aa408b6e5fe8d4aafe
monkuma <kumamoto.jobs@gmail.com>
2021-05-27 16:15:31 +0900

    update

--- learning-web/src/servlet/LoginServlet.java
+++ learning-web/src/servlet/LoginServlet.java
@@ -30,7 +30,7 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("pageName", "Login Form");
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/public/LoginForm.jsp");
 		dispatcher.forward(request, response);
 
