e6496d1295ed4186c4b6d95715c08f5348ba523e
monkuma <kumamoto.jobs@gmail.com>
2021-05-27 19:46:50 +0900

    update RegisterServlet

--- learning-web/src/servlet/RegisterServlet.java
+++ learning-web/src/servlet/RegisterServlet.java
@@ -8,9 +8,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import model.Account;
 import model.RegisterLogic;
+import model.RegisterPageLogic;
 /**
  * Servlet implementation class RegisterServlet
  */
@@ -30,7 +32,26 @@ public class RegisterServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/public/RegisterForm.jsp");
+
+		RequestDispatcher dispatcher = null;
+		String url = request.getParameter("target");
+
+		System.out.println("RegisterServlet URL: " + url);
+		if(url != null) {
+			RegisterPageLogic bo = new RegisterPageLogic();
+			String mail = bo.execute(url);
+			if(mail != null) {
+				HttpSession session = request.getSession();
+				session.setAttribute("mail", mail);
+
+				dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/public/Registration.jsp");
+			}else {
+				dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/public/welcome.jsp");
+			}
+		}else {
+			System.out.println("リンクからURL取得できず、Register Form へフォワード");
+			dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/public/RegisterForm.jsp");
+		}
 
 		dispatcher.forward(request, response);
 
@@ -45,10 +66,12 @@ public class RegisterServlet extends HttpServlet {
 		String mail = request.getParameter("id");
 		String name = request.getParameter("name");
 		String pass = request.getParameter("pass");
+		System.out.println(mail + " "+ name + " "+ pass + " Post RigsterServlet");
 
 		RegisterLogic bo = new RegisterLogic();
 		Account account = new Account(name, pass, mail);
 		boolean result = bo.execute(account);
+		System.out.println(result);
 
 
 		if(result) {
