de471fa6c90183eb93c1148c5d584b81aa3ad87d
monkuma <kumamoto.jobs@gmail.com>
2021-06-03 16:18:45 +0900

    update

--- learning-web/WebContent/js/jdoodle-api.js
+++ learning-web/WebContent/js/jdoodle-api.js
@@ -4,16 +4,18 @@ function sampleAjax(script) {
 	var request = {
 			script : script,
 	};
+
 	//ajaxでservletにリクエストを送信
 	$.ajax({
 		type    : "POST",          //GET / POST
-		url     : "http://localhost:8080/learning-web/CompileServlet",
+//				url     : "http://localhost:8080/learning-web/CompileServlet",
+				url     : "https://learn-java.net/CompileServlet",
 		data    : request,        //リクエストJSON
 		async   : true,           //true:非同期(デフォルト), false:同期
 		success : function(data) {
-			var result = document.getElementById("result-area");
+			var rs = document.getElementById("result-area");
 			console.log("Success");
-			result.innerText = data;
+			rs.innerText = data;
 		},
 		error : function(XMLHttpRequest, textStatus, errorThrown) {
 			alert("リクエスト時になんらかのエラーが発生しました：" + textStatus +":\n" + errorThrown);
