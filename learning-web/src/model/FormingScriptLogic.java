acbed02c4224f141112d3160b68f4ed8be89c576
monkuma <kumamoto.jobs@gmail.com>
2021-06-28 18:39:15 +0900

    fix complieLogic "/n"

--- learning-web/src/model/FormingScriptLogic.java
+++ learning-web/src/model/FormingScriptLogic.java
@@ -2,7 +2,9 @@ package model;
 
 public class FormingScriptLogic {
 	public String execute(String script) {
-		String result = script.replace("\n", "").replace("\"", "\\\"").replace("\t", "");
+		String result = script.replace("\n", "\\n")
+				.replace("\"", "\\\"")
+				.replace("\t", "");
 
 		return result;
 	}
